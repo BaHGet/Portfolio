b8bcbadaf4473c4c7d54c97d28346c27453b45bf
BaHGeT <abosama150@gmail.com>
2022-10-11 23:26:09 +0200

    add mobile style

--- script.js
+++ script.js
@@ -2,40 +2,30 @@ const navLinks = [...document.querySelectorAll('.nav-link')];
 navLinks.map(link =>{
     link.addEventListener('click', () =>{
         const element = document.getElementById(link.getAttribute("data-name"));
+        const menu = document.querySelector(".overly")
+        menu.style.display="none";
+    
         element.scrollIntoView({behavior:'smooth', block:'start'})
-        const oldEle = document.querySelector(".currentpage")
-        if(oldEle){
-            oldEle.classList.remove("currentpage")
-        }
+        const oldEle = [...document.querySelectorAll(".currentpage")]
+        oldEle.map(el =>{
+            if(el){
+                el.classList.remove("currentpage")
+            }
+        })
         link.classList.add("currentpage")
     })
 })
 
+function handleShowMenu(e) {
+    e.preventDefault();
+    const menu = document.querySelector(".overly")
+    console.log(menu)
+    let displayStats = getComputedStyle(menu).getPropertyValue("display");
+    if(displayStats==="none" || menu.style.display==="none")
+    {
+        menu.style="display:flex !important"
+    }else{
+        menu.style="display:none !important"
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*  
-
-
-
-<div id="projects" class="composition">
-    <h1 class="heading">Projects:</h1>
-</div>
-
-<div id="contact" class="composition">
-    <h1 class="heading">Contact me:</h1>
-</div>
-
-
-
-*/
